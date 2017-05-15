c3d4cd003d7df494b16321634c2e6849deb72068
Chris Whong <chris@whong.org>
2017-05-15 17:15:33 -0400

    update RasterSource to include tileSize parameter

--- src/source/RasterSource.jsx
+++ src/source/RasterSource.jsx
@@ -11,17 +11,18 @@ const RasterSource = React.createClass({
   componentWillMount() {
     this.map = this.props.map.mapObject;
     // fetch data if necessary, add layer to map
-    this.addSource(this.props.source.tiles);
+    this.addSource();
   },
 
-  addSource(tiles) {
+  addSource() {
     if (this.map.getSource(this.props.source.id)) {
       this.map.removeSource(this.props.source.id);
     }
 
     this.map.addSource(this.props.source.id, {
       type: 'raster',
-      tiles: [tiles],
+      tiles: [this.props.source.tiles],
+      tileSize: this.props.source.tileSize,
     });
 
     this.props.onLoaded(this.map.getStyle().sources);
